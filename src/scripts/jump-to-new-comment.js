3457fa9473338ea5322e04bfcb3d1d5363463130
theCrius <claudio.vallesi@gmail.com>
2018-06-06 23:45:41 +0100

    Working settings, features load data from local storage

--- src/scripts/jump-to-new-comment.js
+++ src/scripts/jump-to-new-comment.js
@@ -1,26 +1,32 @@
 /* globals $ */
 
-const jumpToNewComment_enabled = window.tildeExtended.settings.jumpToNewComment.enabled;
+chrome.storage.sync.get({
+  tildesExtendedSettings: {jumpToNewComment: {}}
+}, function(res) {
+  // const clog = console.log.bind(console);
+  // clog(res);
+  const jumpToNewComment_enabled = res.tildesExtendedSettings.jumpToNewComment.enabled;
 
-if (jumpToNewComment_enabled) {
-  // If there are new comment, show the button
-  if($('.is-comment-new').length) {
-      $(`
-         <input id="TE_scrollToNewComments" type="button" value="Next New Comment" class="btn btn-primary fixed-bottom-right" />
-       `).appendTo($("body"));
-      $('#TE_scrollToNewComments').on('click', (e) => { __te_scrollToNewComment(e) });
-  }
-}
-
-function __te_scrollToNewComment(e) {
-  e.preventDefault();
-  $('.is-comment-new')[0].scrollIntoView();
-  // Remove the "new comment" class to update the visual feedback
-  setTimeout(function() {
-    $('.is-comment-new')[0].classList.remove('is-comment-new');
-    // Check if there are more new comments. If not, remove the button.
-    if(!$('.is-comment-new').length) {
-      $('#TE_scrollToNewComments').remove();
+  if (jumpToNewComment_enabled) {
+    // If there are new comment, show the button
+    if($('.is-comment-new').length) {
+        $(`
+           <input id="TE_scrollToNewComments" type="button" value="Next New Comment" class="btn btn-primary fixed-bottom-right" />
+         `).appendTo($("body"));
+        $('#TE_scrollToNewComments').on('click', (e) => { __te_scrollToNewComment(e) });
     }
-  }, 250);
-}
+  }
+
+  function __te_scrollToNewComment(e) {
+    e.preventDefault();
+    $('.is-comment-new')[0].scrollIntoView();
+    // Remove the "new comment" class to update the visual feedback
+    setTimeout(function() {
+      $('.is-comment-new')[0].classList.remove('is-comment-new');
+      // Check if there are more new comments. If not, remove the button.
+      if(!$('.is-comment-new').length) {
+        $('#TE_scrollToNewComments').remove();
+      }
+    }, 250);
+  }
+});
--- src/scripts/link-new-tab.js
+++ src/scripts/link-new-tab.js
@@ -1,39 +1,41 @@
-/* globals $, clog */
+/* globals $ */
 
-const linkNewTab_enabled = window.tildeExtended.settings.linkNewTab.enabled;
-const linkNewTab_type = window.tildeExtended.settings.linkNewTab.type;
+chrome.storage.sync.get({
+  tildesExtendedSettings: {linkNewTab: {}}
+}, function(res) {
+  // const clog = console.log.bind(console);
+  // clog(res);
+  const linkNewTab_enabled = res.tildesExtendedSettings.linkNewTab.enabled;
+  const linkNewTab_type = res.tildesExtendedSettings.linkNewTab.types;
 
-clog('linkNewTab', linkNewTab_enabled);
-
-/**
- * Everytime the user click on a link, it checks how to behave given the context and the settings
- */
-$('a').click(function() {
-  if (linkNewTab_enabled) {
-    if(linkNewTab_type.findIndex(t => t === 'text_submissions') !== -1) {
-      if($(this).parent().hasClass('topic-title') && $(this).attr('href').indexOf("http") === -1) {
-          $(this).attr('target', '_blank');
+  // Everytime the user click on a link, it checks how to behave given the context and the settings
+  $('a').click(function() {
+    if (linkNewTab_enabled) {
+      if(linkNewTab_type.findIndex(t => t === 'text_submissions') !== -1) {
+        if($(this).parent().hasClass('topic-title') && $(this).attr('href').indexOf("http") === -1) {
+            $(this).attr('target', '_blank');
+        }
+      }
+      if(linkNewTab_type.findIndex(t => t === 'text_submissions_links') !== -1) {
+        if($(this).parent().parent().hasClass('topic-text-excerpt') || $(this).parent().parent().hasClass('topic-full-text')) {
+            $(this).attr('target', '_blank');
+        }
+      }
+      if(linkNewTab_type.findIndex(t => t === 'link_submissions') !== -1) {
+        if($(this).parent().hasClass('topic-title') && $(this).attr('href').indexOf("http") !== -1) {
+            $(this).attr('target', '_blank');
+        }
+      }
+      if(linkNewTab_type.findIndex(t => t === 'comment_links') !== -1) {
+        if($(this).parent().parent().hasClass('comment-text')) {
+            $(this).attr('target', '_blank');
+        }
+      }
+      if(linkNewTab_type.findIndex(t => t === 'users') !== -1) {
+        if($(this).hasClass('link-user')) {
+            $(this).attr('target', '_blank');
+        }
       }
     }
-    if(linkNewTab_type.findIndex(t => t === 'text_submissions_links') !== -1) {
-      if($(this).parent().parent().hasClass('topic-text-excerpt') || $(this).parent().parent().hasClass('topic-full-text')) {
-          $(this).attr('target', '_blank');
-      }
-    }
-    if(linkNewTab_type.findIndex(t => t === 'link_submissions') !== -1) {
-      if($(this).parent().hasClass('topic-title') && $(this).attr('href').indexOf("http") !== -1) {
-          $(this).attr('target', '_blank');
-      }
-    }
-    if(linkNewTab_type.findIndex(t => t === 'comment_links') !== -1) {
-      if($(this).parent().parent().hasClass('comment-text')) {
-          $(this).attr('target', '_blank');
-      }
-    }
-    if(linkNewTab_type.findIndex(t => t === 'users') !== -1) {
-      if($(this).hasClass('link-user')) {
-          $(this).attr('target', '_blank');
-      }
-    }
-  }
+  });
 });
