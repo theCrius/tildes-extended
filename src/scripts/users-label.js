e120afbf81f9ba5324ea1e82b34a8643bf3ae708
theCrius <claudio.vallesi@gmail.com>
2018-06-08 22:48:05 +0100

    User labels adjust the text brigthness automatically
    https://github.com/theCrius/tildes-extended/issues/5

--- src/scripts/users-label.js
+++ src/scripts/users-label.js
@@ -27,6 +27,9 @@ chrome.storage.local.get({
         }
       });
 
+      // Determine dark/bright theme and adjusting the css accordingly
+      $("span[id^=TE_label]").colourBrightness();
+
       // Listener on 'click' for labels
       $("span[id^=TE_label]").on('click', (e) => editLabel(e));
 
@@ -93,6 +96,7 @@ function updateLabel(e) {
       $("[id^='TE_label"+ $("#edit_label_id").val() +"']").removeClass();
       $("[id^='TE_label"+ $("#edit_label_id").val() +"']").addClass('user-label '+ $("#edit_label_color").val());
       $("[id^='TE_label"+ $("#edit_label_id").val() +"']").text($("#edit_label_text").val());
+      $("[id^='TE_label"+ $("#edit_label_id").val() +"']").colourBrightness();
     });
   }
   closeEditBox(e);
@@ -108,6 +112,7 @@ function removeLabel(e) {
       $("[id^='TE_label"+ $("#edit_label_id").val() +"']").removeClass();
       $("[id^='TE_label"+ $("#edit_label_id").val() +"']").addClass('user-label bg-none');
       $("[id^='TE_label"+ $("#edit_label_id").val() +"']").text('+');
+      $("[id^='TE_label"+ $("#edit_label_id").val() +"']").colourBrightness();
     });
   }
   closeEditBox(e);
@@ -117,4 +122,50 @@ function closeEditBox(e) {
   e.preventDefault();
   // clog('Current labels:', labels);
   $("#label_edit").hide();
+}
+
+// ============== Utils ===============
+// This stuff should go into the background.js script and being passed with a message approach
+
+// From https://github.com/jamiebrittain/colourBrightness.js
+$.fn.colourBrightness = function(){
+  function getBackgroundColor($el) {
+    let bgColor = "";
+    while($el[0].tagName.toLowerCase() != "html") {
+      bgColor = $el.css("background-color");
+      if(bgColor != "rgba(0, 0, 0, 0)" && bgColor != "transparent") {
+        break;
+      }
+      $el = $el.parent();
+    }
+    return bgColor;
+  }
+
+  let r,g,b,brightness,
+      colour = getBackgroundColor(this);
+
+  if (colour.match(/^rgb/)) {
+    colour = colour.match(/rgba?\(([^)]+)\)/)[1];
+    colour = colour.split(/ *, */).map(Number);
+    r = colour[0];
+    g = colour[1];
+    b = colour[2];
+  } else if ('#' == colour[0] && 7 == colour.length) {
+    r = parseInt(colour.slice(1, 3), 16);
+    g = parseInt(colour.slice(3, 5), 16);
+    b = parseInt(colour.slice(5, 7), 16);
+  } else if ('#' == colour[0] && 4 == colour.length) {
+    r = parseInt(colour[1] + colour[1], 16);
+    g = parseInt(colour[2] + colour[2], 16);
+    b = parseInt(colour[3] + colour[3], 16);
+  }
+
+  brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  if (brightness < 125) {
+    this.removeClass("text-light").addClass("text-dark");
+  } else {
+    this.removeClass("text-dark").addClass("text-light");
+  }
+  return this;
 }
