7364145effbdcd9a86bc9b8b9631e43b4f635f64
theCrius <claudio.vallesi@gmail.com>
2018-06-08 12:16:05 +0100

    Fix for markdown preview showing up for any input field

--- src/scripts/markdown-preview.js
+++ src/scripts/markdown-preview.js
@@ -13,22 +13,24 @@ chrome.storage.local.get({
       // clog($(e.target).val());
       const $targetArea = $(e.target);
       const $targetParent = $targetArea.parent()
-      if (!$targetArea.parent().has('#markdown-preview-div').length) {
-        $targetArea.parent().append(`
-        <div id="markdown-preview-div">
-          <hr/>
-          <div>
-            <h2>Live preview</h2>
-            <div id="markdown-preview"></div>
-          </div>
-        </div>`);
-      }
+      if($targetArea[0].name === 'markdown') {
+        if (!$targetArea.parent().has('#markdown-preview-div').length) {
+          $targetArea.parent().append(`
+          <div id="markdown-preview-div">
+            <hr/>
+            <div>
+              <h2>Live preview</h2>
+              <div id="markdown-preview"></div>
+            </div>
+          </div>`);
+        }
 
-      if (!$targetArea.val().length) {
-        $targetParent.find('#markdown-preview-div').hide();
-      } else {
-        $targetParent.find('#markdown-preview-div').show();
-        $targetParent.find('#markdown-preview').html(marked($targetArea.val()));
+        if (!$targetArea.val().length) {
+          $targetParent.find('#markdown-preview-div').hide();
+        } else {
+          $targetParent.find('#markdown-preview-div').show();
+          $targetParent.find('#markdown-preview').html(marked($targetArea.val()));
+        }
       }
     }, 250));
   }
