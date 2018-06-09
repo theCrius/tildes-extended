72f599d8d5699d388363014d02f2972ad84b3cfc
theCrius <claudio.vallesi@gmail.com>
2018-06-09 01:31:44 +0100

    Adjust position of jump-to-new-comments and add 'back to top'

--- src/scripts/jump-to-new-comment.js
+++ src/scripts/jump-to-new-comment.js
@@ -6,18 +6,24 @@ chrome.storage.local.get({
   // const clog = console.log.bind(console);
   // clog(res);
   const jumpToNewComment_enabled = res.tildesExtendedSettings.jumpToNewComment.enabled;
+  const inTopic = window.location.pathname !== '/';
 
-  if (jumpToNewComment_enabled) {
+  if (jumpToNewComment_enabled && inTopic) {
     // If there are new comment, show the button
     if($(".is-comment-new").length) {
         $(`<input id="TE_scrollToNewComments"
             type="button"
-            value="Next New Comment"
             class="btn btn-primary fixed-bottom-right">
         `).appendTo($("body"));
     }
 
-    $('#TE_scrollToNewComments').on('click', () => { __te_scrollToNewComment() });
+    if($(".is-comment-new").length) {
+      $("#TE_scrollToNewComments").val('Next New Comment');
+      $('#TE_scrollToNewComments').on('click', () => { __te_scrollToNewComment() });
+    } else {
+      $("#TE_scrollToNewComments").val('Back to Top');
+      $('#TE_scrollToNewComments').on('click', () => { __te_scrollToTop() });
+    }
   }
 });
 
@@ -28,7 +34,13 @@ function __te_scrollToNewComment() {
     $(".is-comment-new")[0].classList.remove('is-comment-new');
     // Check if there are more new comments. If not, remove the button.
     if(!$(".is-comment-new").length) {
-      $("#TE_scrollToNewComments").remove();
+      $("#TE_scrollToNewComments").val('Back to Top');
+      $('#TE_scrollToNewComments').off('click');
+      $('#TE_scrollToNewComments').on('click', () => { __te_scrollToTop() });
     }
   }, 250);
+}
+
+function __te_scrollToTop() {
+  window.scrollTo(0, 0);
 }
