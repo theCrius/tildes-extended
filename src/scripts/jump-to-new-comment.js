096d2d7821e435f6f8c4046e04511305800af4ab
theCrius <claudio.vallesi@gmail.com>
2018-06-11 12:39:34 +0100

    Add smooth scrolling and back to top button for topics

--- src/scripts/jump-to-new-comment.js
+++ src/scripts/jump-to-new-comment.js
@@ -1,46 +1,71 @@
 /* globals $ */
+// const clog = console.log.bind(console);
 
 chrome.storage.local.get({
   tildesExtendedSettings: {jumpToNewComment: {}}
 }, function(res) {
-  // const clog = console.log.bind(console);
   // clog(res);
   const jumpToNewComment_enabled = res.tildesExtendedSettings.jumpToNewComment.enabled;
   const inTopic = window.location.pathname !== '/';
 
   if (jumpToNewComment_enabled && inTopic) {
-    // If there are new comment, show the button
-    if($(".is-comment-new").length) {
-        $(`<input id="TE_scrollToNewComments"
-            type="button"
-            class="btn btn-primary fixed-bottom-right">
-        `).appendTo($("body"));
-    }
+    $(`<input id="TE_scrollToNewComments"
+        type="button"
+        class="btn btn-primary fixed-bottom-right">
+    `).appendTo($("body"));
+    $("#TE_scrollToNewComments").hide();
 
-    if($(".is-comment-new").length) {
-      $("#TE_scrollToNewComments").val('Next New Comment');
-      $('#TE_scrollToNewComments').on('click', () => { __te_scrollToNewComment() });
-    } else {
-      $("#TE_scrollToNewComments").val('Back to Top');
-      $('#TE_scrollToNewComments').on('click', () => { __te_scrollToTop() });
-    }
+    chooseButton();
   }
 });
 
-function __te_scrollToNewComment() {
-  $(".is-comment-new")[0].scrollIntoView();
-  // Remove the "new comment" class to update the visual feedback
-  setTimeout(function() {
-    $(".is-comment-new")[0].classList.remove('is-comment-new');
-    // Check if there are more new comments. If not, remove the button.
-    if(!$(".is-comment-new").length) {
-      $("#TE_scrollToNewComments").val('Back to Top');
-      $('#TE_scrollToNewComments').off('click');
-      $('#TE_scrollToNewComments').on('click', () => { __te_scrollToTop() });
-    }
-  }, 250);
+function chooseButton() {
+  if($(".is-comment-new").length) {
+    $("#TE_scrollToNewComments").val('Next New Comment');
+    $('#TE_scrollToNewComments').on('click', (e) => { __te_scrollToNewComment(e) });
+    $("#TE_scrollToNewComments").show();
+  } else {
+    $("#TE_scrollToNewComments").val('Back to Top');
+    $('#TE_scrollToNewComments').off('click');
+    $('#TE_scrollToNewComments').on('click', (e) => { __te_scrollToTop(e) });
+    if ($(window).scrollTop() > 250)
+      $("#TE_scrollToNewComments").show();
+
+    backTopListener();
+  }
 }
 
-function __te_scrollToTop() {
-  window.scrollTo(0, 0);
+function backTopListener() {
+  $(window).on('scroll', () => {
+    if ($(window).scrollTop() > 250) {
+      $("#TE_scrollToNewComments").show();
+    } else {
+      $("#TE_scrollToNewComments").hide();
+    }
+  });
+}
+
+function __te_scrollToNewComment(e) {
+  e.preventDefault();
+  const $newComment = $(".is-comment-new").first();
+  $("#TE_scrollToNewComments").attr('disabled', true);
+  // clog('Scrolling to', $newComment.attr('id'));
+  $("html, body").animate({ scrollTop: $newComment.offset().top }, 250, () => {
+    // clog('Removed new from:', $newComment.attr('id'));
+    $newComment.removeClass('is-comment-new');
+    $("#TE_scrollToNewComments").attr('disabled', false);
+    if(!$(".is-comment-new").length) {
+      // clog('no more new comments:', $(".is-comment-new"));
+      chooseButton();
+    }
+  });
+}
+
+function __te_scrollToTop(e) {
+  e.preventDefault();
+  $("#TE_scrollToNewComments").attr('disabled', true);
+  $("html, body").animate({ scrollTop: 0 }, 500, () => {
+    $("#TE_scrollToNewComments").attr('disabled', false);
+    backTopListener();
+  });
 }
