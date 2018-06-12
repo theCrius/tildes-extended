3d5f29dbadd98af205481b266d3452139897cc1d
theCrius <claudio.vallesi@gmail.com>
2018-06-12 13:20:52 +0100

    Fix typos, streamline logic, working version synchronous saving
    I don't really like using $.ajax synchronously as it locks the page but it makes for an easier code to read.

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -20,9 +20,8 @@ const defaultSettings = {
   customStyles: {
     enabled: false,
     urls: [],
-    externalCss: '',
     customCss: '',
-    source: null
+    source: ''
   },
   markdownPreview: {
     enabled: true
@@ -62,44 +61,41 @@ function loadOptions() {
     $('#link_new_tab_type_users').prop("checked", config.tildesExtendedSettings.linkNewTab.types.findIndex(i => i === 'users') !== -1);
     // Jump to New Comment
     $('#jump_new_comment_enabled').prop("checked", config.tildesExtendedSettings.jumpToNewComment.enabled);
-    // Load Custom Styles
-    $('#custom_styles_enabled').prop("checked", config.tildesExtendedSettings.customStyles.enabled);
-    $('#custom_styles_urls').val(config.tildesExtendedSettings.customStyles.urls.join(','));
-    $('#custom_styles_textarea').val(config.tildesExtendedSettings.customStyles.customCss);
-    if($('#custom_styles_enabled').prop("checked", config.tildesExtendedSettings.customStyles.enabled)) {
-      $('#custom_styles_urls').attr('disabled', false);
-      $('#custom_styles_textarea').attr('disabled', false);
-      
-      $('#custom_styles_urls').val('');
-      $('#custom_styles_textarea').val('');
-    }
-    $('#custom_styles_enabled').change(function() {
-        if ($(this).is(':checked')) {
-          $('#custom_styles_urls').attr('disabled', false);
-          $('#custom_styles_textarea').attr('disabled', false);
-        } else {
-          $('#custom_styles_urls').attr('disabled', true);
-          $('#custom_styles_textarea').attr('disabled', true);
-          
-          $('#custom_styles_urls').val('');
-          $('#custom_styles_textarea').val('');
-        }
-    });
     // Markdown Preview
     $('#markdown_preview_enabled').prop("checked", config.tildesExtendedSettings.markdownPreview.enabled);
     // Users Label
     $('#users_label_enabled').prop("checked", config.tildesExtendedSettings.usersLabel.enabled);
     // Sticky Header
     $('#sticky_header_enabled').prop("checked", config.tildesExtendedSettings.stickyHeader.enabled);
+    // Load Custom Styles
+    $('#custom_styles_enabled').prop("checked", config.tildesExtendedSettings.customStyles.enabled);
+    $('#custom_styles_urls').val(config.tildesExtendedSettings.customStyles.urls.join(','));
+    $('#custom_styles_textarea').val(config.tildesExtendedSettings.customStyles.customCss);
+    if($('#custom_styles_enabled').prop("checked", config.tildesExtendedSettings.customStyles.enabled)) {
+      $('#custom_styles_urls').attr('disabled', false);
+      $('#custom_styles_local').attr('disabled', false);
+
+      $('#custom_styles_urls').val(config.tildesExtendedSettings.customStyles.urls.join(', '));
+      $('#custom_styles_local').val(config.tildesExtendedSettings.customStyles.localCss);
+    }
+    $('#custom_styles_enabled').change(function() {
+        if ($(this).is(':checked')) {
+          $('#custom_styles_urls').attr('disabled', false);
+          $('#custom_styles_local').attr('disabled', false);
+        } else {
+          $('#custom_styles_urls').attr('disabled', true);
+          $('#custom_styles_local').attr('disabled', true);
+
+          $('#custom_styles_urls').val('');
+          $('#custom_styles_local').val('');
+        }
+    });
   });
 }
 
 function saveOptions() {
   $('#options_save').attr('disabled', true);
-
-  var stylesheetUrls =  $('#custom_styles_urls').val().split(',').map(function(url) {
-    return url.trim();
-  });
+  updateStatus('Saving...', 'loading');
 
   const options = {};
   options.linkNewTab = {
@@ -109,62 +105,64 @@ function saveOptions() {
   options.jumpToNewComment = {
     enabled: $('#jump_new_comment_enabled').prop('checked')
   };
-  options.customStyles = {
-    enabled: $('#custom_styles_enabled').prop('checked'),
-    localCss: $('#custom_styles_local').val(),
-    urls: stylesheetUrls
-  };
   options.markdownPreview = {
     enabled: $('#markdown_preview_enabled').prop("checked")
   };
+  options.stickyHeader = {
+    enabled: $('#sticky_header_enabled').prop("checked")
+  }
   options.usersLabel = {
     enabled: $('#users_label_enabled').prop("checked")
   };
-  // TODO: This could still be cleaned up more
+  options.customStyles = {
+    enabled: $('#custom_styles_enabled').prop('checked'),
+    localCss: $('#custom_styles_local').val(),
+    urls: $('#custom_styles_urls').val().replace(/\s/g,'').split(','),
+  };
+
   if (options.customStyles.enabled) {
-    updateStatus('Saving...', 'loading');
-
-    if (options.customStyles.urls !== undefined && options.customStyles.urls.length != 0) {
-      downloadStylesheets(options.customStyles.urls)
+    if (options.customStyles.urls.length) {
+      // Add custom user CSS sources
+      options.customStyles.source = options.customStyles.localCss.length ? options.customStyles.localCss : '';
+      //Add external resources
+      const urlSource = buildStylesheets(options.customStyles.urls)
+      if (urlSource.type === 'error') {
+        updateStatus('Error with remote stylesheets: <br>' + urlSource.message, 'failure', 10000);
+      } else {
+        options.customStyles.source += urlSource;
+        storeConfig(options);
+      }
+    } else {
+      storeConfig(options);
     }
-
-    // Combine external and local CSS into single source sheet
-    options.customStyles.source = options.customStyles.externalCss + "\n" + options.customStyles.localCss;
   } else {
     options.customStyles.source = null;
+    storeConfig(options);
   }
-
-  storeConfig(options);
 }
 
-function downloadStylesheets(stylsheetUrls) {
-  $.when.apply($, stylesheetUrls.map(function(url) {
-      return $.ajax(url);
-  })).done(function() {
-      var stylesheets = [];
-      
-      for (var i = 0; i < arguments.length; i++) {
-        stylesheets.push(arguments[i][0]);
+// Reach for remote resources and build the CSS to inject
+// TODO: Use a synchronous approach. Everyone welcome to make this asynchronous
+// TODO: just remember that saveOptions() as well will need to change to support it
+function buildStylesheets(urls) {
+  try {
+    let externalSources = '';
+    // Concatenate all external CSS sources
+    if (urls.length) {
+      for (let i = 0; i < urls.length; i++) {
+        const res = $.ajax(urls[i], {'async': false});
+        // clog('RES:', res.status, res.responseText)
+        if (200 <= res.status && res.status < 300) {
+          externalSources += '\r\n\r\n' + res.responseText;
+        } else {
+          throw `${urls[i]} returned: ${res.statusText} (${res.status})`;
+        }
       }
-
-      options.customStyles.localCss = stylesheets.join("\r\n");
-  }).fail(function(error) {
-    $('#options_save').attr('disabled', false);
-    updateStatus('Error downloading stylesheets, please check the URLs and try again. Multiple URLs should be separated by commas.', 'failure', 3000);
-    clog('ERROR LOADING CUSTOM STYLE:', err.statusText);
-  });
-}
-
-function updateStatus(message, cssClass, removeAfter = false) {
-  $('#options_status').removeClass();
-  $('#options_status').addClass(cssClass);
-  $('#options_status').html(message);
-
-  if(removeAfter) {
-    setTimeout(function() {
-      $('#options_status').removeClass();
-      $('#options_status').html('');
-    }, removeAfter);
+    }
+    return externalSources;
+  } catch(err) {
+    // clog('something wrong in building the styles:', err);
+    return {'type': 'error', 'message': err};
   }
 }
 
@@ -176,8 +174,22 @@ function storeConfig(options) {
     clog('Config updated:', options);
 
     $('#options_save').attr('disabled', false);
-    updateStatus('Options Saved!<br>Remember to refresh Tildes.net for the changes to take effect!', 'success', 3000)
+    updateStatus('Options Saved!<br>Remember to refresh Tildes.net for the changes to take effect!', 'success', 6000)
   });
+}
+
+// Manage Feedback to the user
+function updateStatus(message, cssClass, removeAfter = false) {
+  $('#options_status').removeClass();
+  $('#options_status').addClass(cssClass);
+  $('#options_status').html(message);
+
+  if(removeAfter) {
+    setTimeout(function() {
+      $('#options_status').removeClass();
+      $('#options_status').html('');
+    }, removeAfter);
+  }
 }
 
 $('#options_save').on('click', saveOptions);
